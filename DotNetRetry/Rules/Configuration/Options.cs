e892300858eb341a2b0850352d4e2a6cfc73c948
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-12-28 16:54:43 +0000

    Added rule options [ci skip]

---
+++ DotNetRetry/Rules/Configuration/Options.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNetRetry.Rules.Configuration
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Options(int attempts, TimeSpan time)
+        {
+            Attempts = attempts;
+            Time = time;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Attempts { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TimeSpan Time { get; }
+    }
+}
---
+++ DotNetRetry/Rules/Configuration/RuleOptions.cs
@@ -0,0 +1,48 @@
+﻿namespace DotNetRetry.Rules.Configuration
+{
+    using System;
+    using Bytes2you.Validation;
+
+    /// <summary>
+    /// Options for <see cref="Rule"/> retry object.
+    /// </summary>
+    public class RuleOptions
+    {
+        private readonly Rule _rule;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rule"></param>
+        public RuleOptions(Rule rule)
+        {
+            _rule = rule;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal int Attempts { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal TimeSpan Time { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public virtual Rule Config(Options config)
+        {
+            Guard.WhenArgument(config, nameof(config))
+                .IsNull()
+                .Throw();
+
+            Attempts = config.Attempts;
+            Time = config.Time;
+            return _rule;
+        }
+    }
+}
