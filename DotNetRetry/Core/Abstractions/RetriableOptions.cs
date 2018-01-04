dcb5a7dbdb5bb3b68b9e3b1907e87b8c20237bd9
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-01-04 20:00:45 +0000

    Moved Options to RetriableOptions.cs Retriable partial

---
+++ DotNetRetry/Core/Abstractions/RetriableOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetRetry.Core.Abstractions
+{
+    using Rules.Configuration;
+
+    /// <summary>
+    /// A base class for retriable.
+    /// </summary>
+    public abstract partial class Retriable
+    {
+        /// <summary>
+        /// The options for specified retriable.
+        /// </summary>
+        internal RuleOptions Options;
+    }
+}
