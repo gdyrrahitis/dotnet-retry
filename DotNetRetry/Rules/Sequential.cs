540028860494d671b75e8bb3fe74f2ab135b1b18
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-01-04 20:03:54 +0000

    Renamed abstract class and used for each retry policy

--- DotNetRetry/Rules/Sequential.cs
+++ DotNetRetry/Rules/Sequential.cs
@@ -4,74 +4,20 @@ using DotNetRetry.Core.Auxiliery;
 [assembly: InternalsVisibleTo(Constants.TestProject)]
 namespace DotNetRetry.Rules
 {
-    using System;
-    using Core;
     using Core.Abstractions;
     using Templates.Sequential;
-    using static Guards;
 
     /// <summary>
     /// A sequential retry technique.
     /// </summary>
-    internal class Sequential : IRetry
+    internal class Sequential : Policy
     {
-        private readonly Retriable _retriable;
-        private readonly ActionBody _actionBody;
-        private readonly FunctionBody _functionBody;
-
         /// <summary>
         /// Initializes a new instance of <see cref="Sequential"/> object.
         /// </summary>
         /// <param name="retriable">A <see cref="Retriable"/> object with global rules.</param>
-        internal Sequential(Retriable retriable)
+        internal Sequential(Retriable retriable) : base(retriable, new ActionBody(retriable), new FunctionBody(retriable))
         {
-            _retriable = retriable;
-            _actionBody = new ActionBody(retriable);
-            _functionBody = new FunctionBody(retriable);
-        }
-
-        /// <summary>
-        /// Attempts to retry an action.
-        /// </summary>
-        /// <param name="action">The action to try execute.</param>
-        /// <exception cref="AggregateException">All exceptions logged from action(s) executed.</exception>
-        public void Attempt(Action action) => Do(() =>
-        {
-            action();
-            _retriable.OnAfterRetryInvocation();
-        });
-
-        /// <summary>
-        /// Retries an action and if something happens stores the exceptions to aggregate them.
-        /// </summary>
-        private void Do(Action action)
-        {
-            ValidateArguments(_retriable.Options.Attempts, _retriable.Options.Time);
-            _actionBody.Attempt(action);
-        }
-
-        /// <summary>
-        /// Attempts to retry an a method that returns a result.
-        /// </summary>
-        /// <typeparam name="T">The type of the return value the action returns.</typeparam>
-        /// <param name="function">The function to try execute.</param>
-        /// <exception cref="AggregateException">All exceptions logged from action(s) executed.</exception>
-        /// <returns>The function return value</returns>
-        public T Attempt<T>(Func<T> function) =>
-            Do(() =>
-            {
-                var result = function();
-                _retriable.OnAfterRetryInvocation();
-                return result;
-            });
-
-        /// <summary>
-        /// Retries an action and if something happens stores the exceptions to aggregate them.
-        /// </summary>
-        private T Do<T>(Func<T> function)
-        {
-            ValidateArguments(_retriable.Options.Attempts, _retriable.Options.Time);
-            return _functionBody.Attempt(function);
         }
     }
 }
