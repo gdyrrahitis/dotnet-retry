9a930309bb58343210da959120026d1c6149aaa3
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-03-03 20:09:03 +0000

    Updated path for nuget on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ ApiKey=$1
 Source=$2
 Version=$3
 
-nuget pack ./DotNetRetry.csproj -MsbuildPath /usr/lib/mono/msbuild/15.0/bin/ -Prop Configuration=Release -Version $Version -Verbosity detailed -OutputDirectory .
+nuget pack ./DotNetRetry/DotNetRetry.csproj -MsbuildPath /usr/lib/mono/msbuild/15.0/bin/ -Prop Configuration=Release -Version $Version -Verbosity detailed -OutputDirectory .
 nuget push ./gdyrra.dotnet.retry.$Version.nupkg -ApiKey $ApiKey -Source $Source
