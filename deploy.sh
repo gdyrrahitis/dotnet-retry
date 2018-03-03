2b81999ff97dc4d650e0e73e24b521ec3350bad5
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-03-03 19:10:12 +0000

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ ApiKey=$1
 Source=$2
 Version=$3
 
-nuget pack -Prop Configuration=Release -Version $Version -Verbosity detailed -OutputDirectory .
+nuget pack DotNetRetry.csproj -Prop Configuration=Release -Version $Version -Verbosity detailed -OutputDirectory .
 nuget push gdyrra.dotnet.retry.$Version.nupkg -ApiKey $ApiKey -Source $Source
